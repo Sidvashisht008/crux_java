24a5459066af120a615e41d124f2a4a9930667af
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-25 02:47:17 +0530

    Create pattern6.java

---
+++ Lecture2/pattern6.java
@@ -0,0 +1,31 @@
+package github_lecture2;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		int n=5;
+		int nst=n;
+		int nsp=0;
+		int rows=1;
+		while(rows<=n)
+		{
+			int csp=1;
+			while(csp<=nsp)
+			{
+				System.out.print("  ");
+				csp++;
+			}
+			int cst=1;
+			while(cst<=nst)
+			{
+				System.out.print("* ");
+				cst++;
+			}
+			System.out.println();
+			nst--;
+			nsp+=2;
+			rows++;
+		}
+
+	}
+}
