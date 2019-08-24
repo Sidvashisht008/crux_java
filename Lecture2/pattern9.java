4b48af8ab16040aca01dab34485503e22f99864f
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-25 02:47:59 +0530

    Create pattern9.java

---
+++ Lecture2/pattern9.java
@@ -0,0 +1,31 @@
+package github_lecture2;
+
+public class pattern9 {
+
+	public static void main(String[] args) {
+		int n=5;
+		int nst=1;
+		int nsp=n-1;
+		int rows=1;
+		while(rows<=n)
+		{
+			int csp=1;
+			while(csp<=nsp)
+			{
+				System.out.print(" ");
+				csp++;
+			}
+			int cst=1;
+			while(cst<=nst)
+			{
+				System.out.print("*");
+				cst++;
+			}
+			System.out.println();
+			rows++;
+			nsp--;
+			nst+=2;
+		}
+
+	}
+}
