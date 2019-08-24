20ad7c54da598d7a9da755105765d95d22c9c04c
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-08-25 02:52:03 +0530

    Create pattern23.java

---
+++ Lecture2/pattern23.java
@@ -0,0 +1,30 @@
+package github_lecture2;
+
+public class pattern23 {
+
+	public static void main(String[] args) {
+		int n=5;
+		int nst=1;
+		int nsp=n-1;
+		int rows=1;
+		while(rows<=n)
+		{
+			int csp=1;
+			while(csp<=nsp)
+			{
+				System.out.print(" ");
+				csp++;
+			}
+			int cst=1;
+			while(cst<=nst)
+			{
+				System.out.print("1");	
+				cst++;
+			}
+			System.out.println();
+			nst+=2;
+			nsp--;
+			rows++;
+		}
+	}
+}
