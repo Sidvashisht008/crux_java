5d4d90a8a10abd2b9ce4c1d0dea7aa34e930d2cb
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-09-07 22:11:54 +0530

    Update reverse_array.java

--- Lecture_6/reverse_array.java
+++ Lecture_6/reverse_array.java
@@ -3,12 +3,29 @@ package github_lecture6;
 public class reverse_array {
 
 	public static void main(String[] args) {
-		int[] A = { 1, 2, 3, 4, 5 };
+		int[] A = { 1, 2, 3, 4, 5, 6 };
 		reverse(A);
 		for (int val : A) {
 			System.out.print(val + " ");
 		}
+		System.out.println();
+		reverse2(A, 0, 2);
+		for (int val : A) {
+			System.out.print(val + " ");
+		}
 
+	}
+
+	public static void reverse2(int[] arr, int i, int j) {
+		int lo = i;
+		int hi = j;
+		while (lo < hi) {
+			int temp = arr[lo];
+			arr[lo] = arr[hi];
+			arr[hi] = temp;
+			lo++;
+			hi--;
+		}
 	}
 
 	public static void reverse(int[] arr) {
