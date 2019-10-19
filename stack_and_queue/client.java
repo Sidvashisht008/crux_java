4ffe7e10355bb220203e2248c18a0cc9b24c4675
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-10-19 22:06:20 +0530

    Create client.java

---
+++ stack_and_queue/client.java
@@ -0,0 +1,108 @@
+package gtihub_lecture16;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		stack s = new stack();
+		queue q = new queue();
+		circular_queue cq = new circular_queue();
+		dynamic_stack ds = new dynamic_stack();
+		dynamic_queue dq = new dynamic_queue();
+//		s.push(10);
+//		s.push(20);
+//		s.push(30);
+//		s.push(40);
+//		s.push(50);
+//		s.pop();
+//		s.display();
+//		s.pop();
+//		System.out.println();
+//		s.pop();
+//		s.pop();
+//		s.pop();
+//		s.pop();
+//		s.pop();
+//		s.display();
+		
+		
+		
+		
+		
+//		q.enqueue(10);
+//		q.enqueue(20);
+//		q.enqueue(30);
+//		q.enqueue(40);
+//		q.enqueue(50);
+//		System.out.println();
+//		q.display();
+//		q.dequeue();
+//		System.out.println();
+//		q.display();
+//		q.dequeue();
+//		q.dequeue();
+//		q.dequeue();
+//		q.dequeue();
+//		q.dequeue();
+		
+		
+		
+		
+		
+		
+//		cq.enqueue(10);
+//		cq.enqueue(20);
+//		cq.enqueue(30);
+//		cq.enqueue(40);
+//		cq.enqueue(50);
+//		System.out.println();
+//		cq.display();
+//		System.out.println();
+//		cq.dequeue();
+//		cq.dequeue();
+//		cq.enqueue(200000);
+//		cq.enqueue(300000);
+//		cq.display();
+	
+		
+		
+		
+		
+		
+		
+//		ds.push(10);
+//		ds.push(20);
+//		ds.push(30);
+//		ds.push(40);
+//		ds.push(50);
+//		ds.push(60);
+//		ds.push(70);
+//		ds.push(80);
+//		ds.push(90);
+//		ds.push(100);
+//		ds.display();
+//		System.out.println();
+//		ds.pop();
+//		ds.display();
+//		ds.pop();
+//		System.out.println();
+//		ds.display();
+		
+		
+		
+		
+		
+//		dq.enqueue(10);
+//		dq.enqueue(20);
+//		dq.enqueue(30);
+//		dq.enqueue(40);
+//		dq.enqueue(50);
+//		dq.enqueue(60);
+//		dq.enqueue(70);
+//		dq.dequeue();
+//		dq.enqueue(1000);
+//		dq.dequeue();	
+//		System.out.println();
+//		dq.display();
+	}
+}
