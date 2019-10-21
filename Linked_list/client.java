22829f004920490900e3970baa7a89e8a8b2b078
Siddharth Vashisht <31439718+Sidvashisht008@users.noreply.github.com>
2019-10-21 18:16:06 +0530

    Create client.java

---
+++ Linked_list/client.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		linked_list ll = new linked_list();
+		ll.addLast(10);
+		ll.addLast(20);
+		ll.addLast(30);
+		ll.addLast(40);
+		ll.addFirst(50);
+		ll.addLast(100);
+//		System.out.println(ll.head);
+//		ll.display();
+	}
+
+}
